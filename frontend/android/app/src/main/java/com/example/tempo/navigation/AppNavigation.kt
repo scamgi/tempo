8cf4ffe5a71206b88e63b2ef7437c5e26cdfd2c7
Giacomo Scampini <scampini.giacomo@gmail.com>
2025-11-13 15:48:07 +0100

    fix: fix some imports in AppNavigation.kt

--- frontend/android/app/src/main/java/com/example/tempo/navigation/AppNavigation.kt
+++ frontend/android/app/src/main/java/com/example/tempo/navigation/AppNavigation.kt
@@ -14,6 +14,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import com.example.tempo.TempoApp
 import com.example.tempo.data.AuthRepository
